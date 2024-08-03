20c78499be669cb236eef2b79de9cce06416076b
kwinkich <vankord21@gmail.com>
2024-08-04 00:08:10 +0300

    test

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,26 +1,26 @@
-window.onload = () => {
-	const isVerified = localStorage.getItem('isVerified'); // Предполагается, что значение хранится в localStorage
-	const currentUrl = window.location.pathname; // Получаем текущий путь
+// window.onload = () => {
+// 	const isVerified = localStorage.getItem('isVerified'); // Предполагается, что значение хранится в localStorage
+// 	const currentUrl = window.location.pathname; // Получаем текущий путь
 
-	if (
-		isVerified === 'true' &&
-		!currentUrl.includes('/pages/app.html') &&
-		!currentUrl.includes('/pages/success.html') &&
-		!currentUrl.includes('/pages/createToDo.html')
-	) {
-		window.location.href = '/pages/app.html';
-	} else if (isVerified !== 'true' && !currentUrl.includes('/index.html')) {
-		window.location.href = '../index.html';
-	}
-};
+// 	if (
+// 		isVerified === 'true' &&
+// 		!currentUrl.includes('/pages/app.html') &&
+// 		!currentUrl.includes('/pages/success.html') &&
+// 		!currentUrl.includes('/pages/createToDo.html')
+// 	) {
+// 		window.location.href = '/pages/app.html';
+// 	} else if (isVerified !== 'true' && !currentUrl.includes('/index.html')) {
+// 		window.location.href = '../index.html';
+// 	}
+// };
 
-function goToApp() {
-	window.location.href = '/pages/app.html';
-}
+// function goToApp() {
+// 	window.location.href = '/pages/app.html';
+// }
 
-function goToCreateToDo() {
-	window.location.href = 'createToDo.html';
-}
-function goToHome() {
-	window.location.href = 'app.html';
-}
+// function goToCreateToDo() {
+// 	window.location.href = 'createToDo.html';
+// }
+// function goToHome() {
+// 	window.location.href = 'app.html';
+// }
