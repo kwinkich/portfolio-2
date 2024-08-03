6cec2e83d43c3cc19b3ca89b251b07ab1ea48384
kwinkich <vankord21@gmail.com>
2024-08-03 20:10:34 +0300

    added some ui

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -2,7 +2,11 @@ window.onload = () => {
 	const isVerified = localStorage.getItem('isVerified'); // Предполагается, что значение хранится в localStorage
 	const currentUrl = window.location.pathname; // Получаем текущий путь
 
-	if (isVerified === 'true' && !currentUrl.includes('/pages/app.html')) {
+	if (
+		isVerified === 'true' &&
+		!currentUrl.includes('/pages/app.html') &&
+		!currentUrl.includes('/pages/success.html')
+	) {
 		window.location.href = '/pages/app.html';
 	} else if (isVerified !== 'true' && !currentUrl.includes('/index.html')) {
 		window.location.href = '../index.html';
